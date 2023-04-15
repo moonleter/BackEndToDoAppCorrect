04c38d3335f2fa536efcf76fe8bb2b86a059bb23
Simon Kunz <r21291@student.osu.cz>
2023-04-15 20:59:48 +0200

    Cors Mapping

--- src/main/java/com/osu/BackEndToDoApp/BackEndToDoAppApplication.java
+++ src/main/java/com/osu/BackEndToDoApp/BackEndToDoAppApplication.java
@@ -2,6 +2,10 @@ package com.osu.BackEndToDoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackEndToDoAppApplication {
@@ -9,6 +13,19 @@ public class BackEndToDoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndToDoAppApplication.class, args);
 
+
+
+
 	}
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
+	}
+
 
 }
